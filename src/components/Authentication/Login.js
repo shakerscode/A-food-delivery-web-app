62085b737c7f2bc72abab9036117e37e5c2191c7
Shaker Ahamed <ahamedshaker7@gmail.com>
2022-07-26 15:19:27 +0600

    register and sign designed done

--- src/components/Authentication/Login.js
+++ src/components/Authentication/Login.js
@@ -2,10 +2,9 @@
 const Login = () => {
 
     return (
-        <div className='px-5 py-3 flex flex-col justify-center items-center h-[600px]'>
-        <h1 className='text-center text-3xl font-semibold text-gray-900  mb-3'>Log In Please</h1>
-  
-        <div className='flex flex-col justify-between gap-2 w-[60%]'> 
+        <div className='px-5  flex flex-col justify-center items-center h-[600px]'>
+        <h1 className='text-center text-3xl font-semibold text-gray-900 mb-3'>Log In Please</h1> 
+        <div className='flex flex-col justify-between gap-2  w-96'> 
             <label>Email <span className='text-red-600'>*</span></label>
             <input
             placeholder='Enter your email' 
@@ -14,7 +13,7 @@ const Login = () => {
             <input
             placeholder='Enter your password' 
             className='pl-4  outline-none	 border-none w-full h-14 rounded-md text-gray-900' type="password" name="" id="" /> 
-            <div className='flex justify-center items-center gap-2 text-sm text-gray-900'><p>Forget password?<span>Reset please.</span></p></div>
+            <div className='flex justify-center items-center gap-2 text-sm text-gray-900'><p>Forget password?<span className="text-red-500 font-semibold underline ml-2">Reset please</span></p></div>
             <button className='bg-red-500 rounded-md px-3 py-2 mt-10 text-lg w-full text-white hover:bg-white hover:text-gray-900 border border-red-500'  type='submit'>Register</button>
         </div>
   
